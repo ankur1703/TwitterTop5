ac15557c0b41e17580b09eb90d38ab6c82abd9fd
Ankur Yadav <ankur7.9@outlook.com>
2015-11-13 15:03:33 +0000

    Added class description in SQLTutorial class

--- TwitterTop5/src/Tutorials/SQLTutorial.java
+++ TwitterTop5/src/Tutorials/SQLTutorial.java
@@ -1,3 +1,62 @@
+/**
+ *  Tutorial class
+ *  
+ *  Assumes you have a database set up as "SQLTutorial" locally. and it has a table "StudentRecord".
+ *  The structure of the table should be:
+ *  Column name				Data type		Constraints
+	student_id				VARCHAR(7)		NOT NULL PRIMARY KEY
+	stud_first_name 		VARCHAR(15)		NOT NULL
+	stud_last_name			VARCHAR(15)		NOT NULL
+	stud_date_of_birth		DATE			NOT NULL
+	stud_address			VARCHAR(50)		NOT NULL
+	stud_program			VARCHAR(20)		NOT NULL
+	stud_marital_status		INT(2)			NOT NULL
+	stud_country			VARCHAR(20)		NOT NULL
+	
+	Populate this table with some data.
+	SQL query to do all this is given below:	
+
+    -- create a table
+    CREATE TABLE IF NOT EXISTS studentrecord (
+       student_id VARCHAR(7),
+       stud_first_name VARCHAR(15) NOT NULL,
+       stud_last_name VARCHAR(15) NOT NULL,
+       stud_date_of_birth DATE NOT NULL,
+       stud_address VARCHAR(50) NOT NULL,
+       stud_program VARCHAR(20) NOT NULL,
+       stud_marital_status INT(2) NOT NULL,
+       stud_country VARCHAR(20) NOT NULL,
+       PRIMARY KEY (student_id)
+    )
+    -- insert a sample data
+    INSERT INTO studentrecord VALUES(
+    '88889','Albukori','Zaman Khan','1969-07-08',
+    '4-5, Dead Wood Street 5, 12000 Sintok, Kedah','MSc. IT',
+    '1','Malaysia');     
+
+    INSERT INTO studentrecord VALUES(
+    '87990','Haslina','Mahathir','1970-11-12',
+    '345, New Smart Village, 17100 Nilai, N. Sembilan','MSc. ICT',
+    '2','Malaysia');    
+
+    INSERT INTO studentrecord VALUES(
+    '79678','Mohammed','Fajr','1975-04-20',
+    'Pearl Apt, Level 10, Al-Hijr, 45200 Abu Dhabi','MSc. Expert System',
+    '2','UEA');     
+
+    INSERT INTO studentrecord VALUES(
+    '88799','Mustar','Mohd Dali','1979-06-24',
+    '345, Side Village, Kerian, 12300 Jawa Barat','MSc. MultiMedia',
+    '1','Indonesia');     
+
+    INSERT INTO studentrecord VALUES(
+    '78998','Satkorn','Chengmo','1968-01-26',
+    '34 Uptown Street #4, Tech Park, 78100 Bangkok','MSc. IT',
+    '2','Thailand');
+
+ */
+
+
 package Tutorials;
 
 import java.sql.Connection;
